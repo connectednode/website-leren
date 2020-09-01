deb376cf8cc52c07bfb185f92bf5e1f5c0497efd
connectednode <connectednode@protonmail.com>
2020-09-01 10:30:49 +0200

    added xml request

--- ajax.js
+++ ajax.js
@@ -1,4 +1,13 @@
 document.getElementById("button").addEventListener('click', loadusers);
 function loadusers() {
-console.log('test');
+console.log('test1');
+var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      
+    }
+  };
+  xmlhttp.open("GET", "https://api.github.com/", true);
+  xmlhttp.send(); 
+console.log('test2');
 }
