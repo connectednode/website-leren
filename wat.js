da80d37a3187b5bbea967eac28747bde6e9dc192
connectednode <connectednode@protonmail.com>
2020-09-07 12:28:25 +0200

    nummer opslag toegevoegt

--- wat.js
+++ wat.js
@@ -1,4 +1,4 @@
-var nummer
+var i
 var nummer1 = {
     numcurent : 0,
     numadd : "",
@@ -7,6 +7,9 @@ var nummer1 = {
         return this.numcurent *= 1
     }
 };
+var nummeropslag = {};
+
+console.log(nummeropslag);
 
 const resultaat = document.getElementById("uit");
 
@@ -14,5 +17,10 @@ function numToVar(x){
     nummer1.numadd = x
     innerHTML = nummer1.test();
     resultaat.innerHTML = nummer1.numcurent
-}
+    console.log(nummer1)
+};
 
+function numToStorag(i=+2, x) {
+    nummeropslag. = nummer1.numcurent
+    nummeropslag. = x
+}
