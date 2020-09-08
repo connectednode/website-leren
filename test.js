21d683438d0a4ffbd3cba6263acc39b532e3bb21
connectednode <connectednode@protonmail.com>
2020-09-08 09:37:53 +0200

    removed files that werent bieng workt on

--- test.js
+++
@@ -1,9 +0,0 @@
-var a
-var b
-var c
-a=1
-b=3
-
-c = a * b
-
-console.log(c)
--- tests/test1.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("num");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("demo").innerHTML = text;
-  }
