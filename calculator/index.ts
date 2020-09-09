8c7d44352dfb7976960a77ec396f83ca3549e399
connectednode <connectednode@protonmail.com>
2020-09-09 10:47:15 +0200

    started on te calculation code

---
+++ calculator/index.ts
@@ -0,0 +1,25 @@
+let x = 1;
+let y = "/";
+let z = 2;
+let a = 0;
+
+function bereken(x: number, y: string, z: number){
+    if(y==="/"){
+    return a = x/z
+    }
+
+    else if(y==="*"){
+    return a = x*y
+    }
+
+    else if(y==="-"){
+    return a = x-y
+    }
+
+    else if(y==="+"){
+    return a = x+y
+    }
+console.log(a)
+};
+
+bereken
