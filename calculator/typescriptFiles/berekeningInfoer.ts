4e9b5ba776453024c2a80536710f9dd2cdb3284e
connectednode <connectednode@protonmail.com>
2020-09-10 11:51:10 +0200

    added numin function

--- calculator/typescriptFiles/berekeningInfoer.ts
+++
@@ -1,4 +0,0 @@
-//type Operator = "/" | "*" | "+" | "-" | "" ;
-//let num: number[] = [0, 0];
-// let op: Operator[] = [""];
-
--- calculator/typescriptFiles/index.ts
+++ calculator/typescriptFiles/index.ts
@@ -4,9 +4,13 @@ let num: number[] = [0, 0];
 
 let op: Operator[] = [""];
 
-let a = 0;
+function numin(inpute: number, w: number){
+    num[w] = inpute
+};
 
 function bereken(i: number){
+    let a = 0;
+
     if(op[i] ==="/"){
     return a = num[i] / num[i++]
     }
@@ -32,3 +36,4 @@ function bereken(i: number){
     }
 
 };
+
