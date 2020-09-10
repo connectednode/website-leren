a98adcae68920276aafbee2c4dc0473d585bc43c
connectednode <connectednode@protonmail.com>
2020-09-10 12:58:34 +0200

    added sorte funcion

--- calculator/typescriptFiles/index.ts
+++ calculator/typescriptFiles/index.ts
@@ -4,8 +4,22 @@ let num: number[] = [0, 0];
 
 let op: Operator[] = [""];
 
+let i: number = 0
+
 function numin(inpute: number, w: number){
     num[w] = inpute
+};
+
+function opin(inpute: Operator, w: number){
+    op[w] = inpute
+};
+
+function sorter1(x: Operator){
+    return op.indexOf(x)
+ };
+
+function sorter(x: Operator){
+   i =  op.indexOf(x)
 };
 
 function bereken(i: number){
@@ -37,3 +51,6 @@ function bereken(i: number){
 
 };
 
+// TODO : aray decoder/sorter Math.min(5, 10);
+// TODO : aray editor
+// TODO : inputorder
