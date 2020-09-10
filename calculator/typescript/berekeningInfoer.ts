c3def2625e6568f2d403b66f2208d9188e3c8fe9
connectednode <connectednode@protonmail.com>
2020-09-10 10:31:55 +0200

    custom type

---
+++ calculator/typescript/berekeningInfoer.ts
@@ -0,0 +1,3 @@
+type Operator = "/" | "*" | "+" | "-" | "" ;
+let num: number[] = [0, 0];
+let op: Operator[] = [""];
--- test.js
+++
@@ -1,6 +0,0 @@
-let global = 1 
-function boe(Var){ return Var + 1 }
-function editGlobal() { global + 1 }
-let newvar = boe(20) // 21
-editGlobal() // global = 2
-editGlobal() // global = 3
