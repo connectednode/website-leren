b33c51f4c234f499c9146b6474a79ced5f79cb1a
connectednode <connectednode@protonmail.com>
2020-09-10 10:34:56 +0200

    moved code to new file

---
+++ calculator/typescript/bereken.ts
@@ -0,0 +1,19 @@
+let a = 0
+function bereken(x: number, y: number, z: string){
+    if(z==="/"){
+    return a = x/y
+    }
+
+    else if(z==="*"){
+    return a = x*y
+    }
+
+    else if(z==="-"){
+    return a = x-y
+    }
+
+    else if(z==="+"){
+    return a = x+y
+    }
+
+};
