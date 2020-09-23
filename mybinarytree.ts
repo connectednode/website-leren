d674713cc13aefc0235af63f9154ddf0e7e076f4
connectednode <connectednode@protonmail.com>
2020-09-23 11:04:49 +0200

    verderd binery tree

--- mybinarytree.ts
+++ mybinarytree.ts
@@ -1,11 +1,14 @@
 // binary tree
 
 // node class
-class BtNode{
-    constructor(calue){
+class node{
+    value: any
+    right: any
+    left: any
+    constructor(value){
         this.value = value 
         this.left = null
-        this.left = null
+        this.right = null
     }
 }
 
@@ -13,5 +16,10 @@ class BtNode{
 
 // tree class
 class BT{
-    
+    root: node
+    count: number
+    constructor(value) {
+        this.root = new node(value)
+        this.count = 1
+    }
 }
