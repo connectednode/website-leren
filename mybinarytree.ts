34e05ed0d6ffb77fc993bd83282118b9805250bf
connectednode <connectednode@protonmail.com>
2020-10-05 10:04:23 +0200

    went back to seperat value and operator variabols

--- mybinarytree.ts
+++ mybinarytree.ts
@@ -2,42 +2,46 @@
 
 // node class
 class node{
-  value: string | number          //TODO
+  value: number          //TODO
   right: any               //TODO
   left: any                //TODO
   index: number            //TODO
-   constructor(value, index){
-
-      if(isNaN(value)){
-        console.log(value)
-      }
+  operator: string
+  isOperator: boolean
+   constructor(value, index, operator, isOperator){
 
        this.value = value  
        this.left = null
        this.right = null
        this.index = index
+       this.operator = operator
+       this.isOperator = isOperator
+
    }
 
-   calculate(){
-     let test = this.value as any
+   calculate(isOperator){  //TODO:make it a loop
+   
+    if(isOperator == false){
+      console.log(this.value)
       return this.value
     }
-  //   else if (this.isOpertor == true){
-  //   if(this.value == "*"){
-  //     this.left.calculate * this.right.calculate
-  //   }
-  //   else if(this.value == "/"){
+    
+    else if (isOperator == true){
+    if(this.operator == "*"){
+     this.value = this.left.calculate * this.right.calculate
+    }
+    else if(this.operator == "/"){
+     this.value = this.left.calculate / this.right.calculate
+    }
+    else if(this.operator == "+"){
+     this.value = this.left.calculate + this.right.calculate
+    }
+    else if(this.operator == "-"){
+     this.value = this.left.calculate - this.right.calculate
+    }
+  }
 
-  //   }
-  //   else if(this.value == "+"){
-
-  //   }
-  //   else if(this.value == "-"){
-
-  //   }
-  // }
-
-  //  }
+   }
 
 }
 
@@ -47,14 +51,14 @@ class node{
 class BT{
    root: node
    count: number
-   constructor(value) {
-       this.root = new node(value, 1)
+   constructor(operator) {
+       this.root = new node(null, 1, operator,true  )
        this.count = 1
    }
 
-  insert(value, index, nodeLeft){
+  insert(value, index, operator, isOperator, nodeLeft){
     this.count++ //TODO: increses the conte of the number of nodes
-    let newNode = new node(value, index)
+    let newNode = new node(value, index, operator, isOperator)
     
     let nodeIndex = "???" //TODO
 
@@ -94,22 +98,22 @@ class BT{
 
 
 
-   changeRoot(value, index){
+   changeRoot(operator){
     let currentRoot
     // currentRoot = bt.bts()  //TODO:add difrent serch?
     console.log(currentRoot)
-    bt = new BT(value)
+    bt = new BT(operator)
     currentRoot.forEach(test)
    };
 }
 
-function test(value, index, nodeLeft){
-  bt.insert(value, index, nodeLeft)
+function test(value, index,operator, isOperator, nodeLeft){
+  bt.insert(value, index,operator, isOperator, nodeLeft)
 }
   
 let bt = new BT(0)
-bt.insert ("+", 2, true)
-bt.insert (1, 3, true)
-bt.insert (2, 4, true)
-bt.insert (4, 5, true)
+bt.insert (null, 2,"+", true, false)
+bt.insert (1, 3, null, false, true)
+bt.insert (2, 4, null, false, true)
+bt.insert (4, 5, null, false, true)
 console.log(bt)
