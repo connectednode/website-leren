b37d5f1b2666b36fae56ab6286060e29f7469dee
connectednode <connectednode@protonmail.com>
2020-10-13 10:53:56 +0200

    beter structering

---
+++ calculator/dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function test() {
+    return console.log("hello world!");
+}
+exports.default = test;
+//# sourceMappingURL=test.js.map
