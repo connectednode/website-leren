576b97fd744176da2ba6dc083cc0398f9441d125
connectednode <connectednode@protonmail.com>
2020-10-15 09:21:44 +0200

    start day

--- calculator/typeschipt/index.ts
+++ calculator/typeschipt/index.ts
@@ -1,8 +1,7 @@
 // import { calculate } from "./calculate";
-import { input  } from "./inpute";
+//import { input  } from "./inpute";
 var output = document.getElementById("output")
 let sum = "2*(5/2+(3-2)/(4**9))"
 console.log(eval(sum))
 output.innerHTML = eval(sum)
-let test = input()
-test
+//let test = input()
